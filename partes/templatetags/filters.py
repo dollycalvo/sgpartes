f4aca6c3355e395c9addb4385651c78967f7c548
Carlos Guimaraenz <carlosguimaraenz@yahoo.com.ar>
2024-02-22 19:42:17 -0300

    Cambio en los días de fin de semana

--- partes/templatetags/filters.py
+++ partes/templatetags/filters.py
@@ -13,3 +13,10 @@ def etiqueta_codigo(value):
 def dia_semana(dia, primerDiaDelMes):
     """Devuelve el día de la semana en base al comienzo y al número de día dado"""
     return diasDeLaSemana[((dia - 1) + primerDiaDelMes) % 7]["corto"]
+
+
+@register.filter
+def class_dia_semana(dia, primerDiaDelMes):
+    """Devuelve una clase CSS de acuerdo al día de la semana en base al comienzo y al número de día dado"""
+    diaSemana = ((dia - 1) + primerDiaDelMes) % 7
+    return "dia-finde" if diaSemana >= 5 else "dia-semana"
