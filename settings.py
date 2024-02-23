d5fa07fdb3302b8b987874b5ed602e31be0a9ae3
dollycalvo <mdcalvo@gmail.com>
2024-02-23 13:25:30 +0000

    pruebas

--- settings.py
+++ settings.py
@@ -132,3 +132,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/home/mdcalvo/sgpartes/partes/static'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de correo (cuentas de gmail)
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "mdcalvogrycn@gmail.com"
+EMAIL_HOST_PASSWORD = "dollycalvo1"
--- sgpartes/settings.py
+++ sgpartes/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de correo (cuentas de gmail)
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "mdcalvogrycn@gmail.com"
+EMAIL_HOST_PASSWORD = "dollycalvo1"
+
