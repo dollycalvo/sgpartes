63058856a003e0874e8bd3308ec2f5cacde6cbed
Carlos Guimaraenz <carlosguimaraenz@yahoo.com.ar>
2024-05-09 20:50:38 -0300

    Nuevos filtros de texto para concatenar

--- partes/templatetags/filters.py
+++ partes/templatetags/filters.py
@@ -1,5 +1,6 @@
 from django import template
 from partes.helper import diasDeLaSemana, etiquetaCodigo
+from partes.views_helpers.common import DIA_LIMITE_PRESENTACION_PLANILLA
 
 register = template.Library()
 
@@ -20,3 +21,15 @@ def class_dia_semana(dia, primerDiaDelMes):
     """Devuelve una clase CSS de acuerdo al día de la semana en base al comienzo y al número de día dado"""
     diaSemana = ((dia - 1) + primerDiaDelMes) % 7
     return "dia-finde" if diaSemana >= 5 else "dia-semana"
+
+
+@register.filter
+def concat(txt1, txt2):
+    """Concatena dos strings"""
+    return str(txt1) + str(txt2)
+
+
+@register.filter
+def concatDiaLimite(txt1):
+    """Concatena dos strings"""
+    return str(txt1) + str(DIA_LIMITE_PRESENTACION_PLANILLA)
